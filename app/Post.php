0655c9bcdcdbee9a7230a1bb4f72746ec39b5afb
MirkOrlando <mirko@orlando-m.it>
2022-06-28 18:45:34 +0200

    create migration add_user_id_to_posts_table

--- app/Post.php
+++ app/Post.php
@@ -21,6 +21,16 @@ class Post extends Model
     }
 
     /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
      * The tags that belong to the Post
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
