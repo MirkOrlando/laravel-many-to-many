3cc9785716de53305415943d58aedb9f0602b206
MirkOrlando <mirko@orlando-m.it>
2022-06-28 16:50:11 +0200

    implement mailable PostCreated

---
+++ app/Mail/PostCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Post;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PostCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $post;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('noreply@example.com')
+            ->subject('A New Post Has Been Added')
+            ->view('view.name');
+    }
+}
